20c99ffe5c4979f65ed3e6341d63a74d63369d36
Cassandra Dunlop <cdunlop@columbia.edu>
2014-08-25 15:02:24 -0400

    update

--- brownfield/brownfield/settings.py
+++ brownfield/brownfield/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'l6-bdte$_e(y#*_cwvba@^13710ar^n#o@#vj*o)5-im9&t)%@'
 
+#setting the template dir inside project folder templates/
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -36,6 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'brownfieldapp',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,28 +60,70 @@ WSGI_APPLICATION = 'brownfield.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brownfieldapp',
+        'HOST': '',
+        'PORT': 5432,
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
+
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'America/New_York'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
+STATIC_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+
+ROOT_URLCONF = 'brownfield.urls'
+
+TEMPLATE_DIRS = (
+    "/var/www/brownfield/templates/",
+    os.path.join(os.path.dirname(__file__), "templates"),
+)
+
+STATICMEDIA_MOUNTS = (
+    ('/sitemedia', 'sitemedia'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+COMPRESS_URL = "/site_media/"
+COMPRESS_ROOT = "media/"
--- brownfield/brownfield/urls.py
+++ brownfield/brownfield/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'brownfield.views.home', name='home'),
+    url(r'^visual_reconnaissance/',
+     TemplateView.as_view(template_name="brownfieldapp/visrecon.html")),
+    # url(r'^$', 'brownfieldapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
